4153673e9e76f76724db33bcded1e4de3ab5e166
Faizal <faizalmalik9633@gmail.com>
2019-08-24 11:02:05 +0530

    [Added] - Initial Project Commit              - Folder structure added              - Pod file added

---
+++ Bitcoin Price/Bitcoin PriceTests/Bitcoin_PriceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bitcoin_PriceTests.swift
+//  Bitcoin PriceTests
+//
+//  Created by Faizal on 8/24/19.
+//  Copyright © 2019 Faizal. All rights reserved.
+//
+
+import XCTest
+@testable import Bitcoin_Price
+
+class Bitcoin_PriceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
